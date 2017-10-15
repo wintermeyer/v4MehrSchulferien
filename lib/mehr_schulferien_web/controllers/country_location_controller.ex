600531e1ca61eb59ce091cd95827ac975ca93730
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2017-10-15 19:08:15 +0200

    Small fixes

--- lib/mehr_schulferien_web/controllers/country_location_controller.ex
+++ lib/mehr_schulferien_web/controllers/country_location_controller.ex
@@ -9,14 +9,19 @@ defmodule MehrSchulferienWeb.CountryLocationController do
   # /countries/:country_id/cities
   #
   def index(conn, %{"country_id" => country_id}) do
+    country = Locations.get_country!(country_id)
+
     query = from cities in City,
+            where: cities.country_id == ^country.id,
             order_by: [cities.name, cities.zip_code],
             select: {cities.zip_code, cities.name, cities.slug}
     cities = Repo.all(query)
 
     federal_states = Locations.list_federal_states
 
-    render(conn, "index_country_cities.html", cities: cities, federal_states: federal_states)
+    render(conn, "index_country_cities.html", cities: cities,
+                                              federal_states: federal_states,
+                                              country: country)
   end
 
 
