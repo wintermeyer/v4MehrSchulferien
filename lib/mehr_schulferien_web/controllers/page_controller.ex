19f2881dc51bfab0438cab7724bf9e93130f0897
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2017-10-17 13:26:51 +0200

    Root page

--- lib/mehr_schulferien_web/controllers/page_controller.ex
+++ lib/mehr_schulferien_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule MehrSchulferienWeb.PageController do
   use MehrSchulferienWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    federal_states = MehrSchulferien.Locations.list_federal_states
+
+    render(conn, "index.html", federal_states: federal_states)
   end
 end
